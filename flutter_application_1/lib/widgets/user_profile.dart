de013191260497da033cc3d63e4ef03dc845eb4d
memphis-cloud <79505122+memphis-cloud@users.noreply.github.com>
2023-10-08 10:55:59 +0300

    telegram layour: add some settings layout

--- flutter_application_1/lib/widgets/user_profile.dart
+++ flutter_application_1/lib/widgets/user_profile.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
-  const UserProfile({super.key});
+  UserProfile({super.key});
+
+  List<MenuRowData> MenuRow = [
+    MenuRowData(Icons.favorite, "Избранное"),
+    MenuRowData(Icons.phone, "Звонки"),
+    MenuRowData(Icons.computer, "Устройства"),
+    MenuRowData(Icons.folder, "Папка с чатами"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,17 +16,33 @@ class UserProfile extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: const Text("Настройки")),
       ),
-      body: UserInfo(),
+      body: Container(
+        width: double.infinity,
+        color: Colors.grey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            UserInfo(),
+            SizedBox(
+              height: 30,
+            ),
+            _MenuWidget(MenuRow: MenuRow),
+          ],
+        ),
+      ),
     );
-    // return const Text("text");
   }
 }
 
 class UserInfo extends StatelessWidget {
+  const UserInfo({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
       child: Column(
         children: [
           SizedBox(
@@ -45,6 +68,8 @@ class UserInfo extends StatelessWidget {
 }
 
 class UserName extends StatelessWidget {
+  const UserName({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Text("Igor <memphis> Rochev");
@@ -52,20 +77,21 @@ class UserName extends StatelessWidget {
 }
 
 class UserImage extends StatelessWidget {
+  const UserImage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 100,
       height: 100,
-      child: Placeholder(
-        fallbackHeight: 100,
-        fallbackWidth: 100,
-      ),
+      child: Placeholder(),
     );
   }
 }
 
 class UserPhone extends StatelessWidget {
+  const UserPhone({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Text("+7(950) 566 31 71");
@@ -73,8 +99,58 @@ class UserPhone extends StatelessWidget {
 }
 
 class UserLogin extends StatelessWidget {
+  const UserLogin({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Text("@memphis");
   }
+}
+
+class _MenuWidget extends StatelessWidget {
+  final List<MenuRowData> MenuRow;
+
+  const _MenuWidget({super.key, required this.MenuRow});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: MenuRow.map((data) => _MenuWidgetRow(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  final MenuRowData data;
+
+  const _MenuWidgetRow({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(data.icon),
+          SizedBox(
+            width: 15,
+          ),
+          Expanded(child: Text(data.text)),
+          Icon(Icons.arrow_right_outlined)
+        ],
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  final IconData icon;
+  final String text;
+
+  MenuRowData(this.icon, this.text);
 }
